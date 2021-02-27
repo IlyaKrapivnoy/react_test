798a8aa814b02d25a898f618aabafeb865b8792d
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-02-27 22:25:00 +0200

    first step

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react'
+import TodoList from './TodoList'
+
+export default function App() {
+
+const [todos, setTodos] = useState([])
+const [todoTitle, setTodoTitle] = useState('')
+
+useEffect(() => {
+  const raw = localStorage.getItem('todos')
+  setTodos(JSON.parse(raw))
+}, [])
+
+useEffect(() => {
+   localStorage.setItem('todos', JSON.stringify(todos))
+}, [todos])
+
+const addTodo = event => {
+  if(event.key === 'Enter') {
+    setTodos([
+      ...todos,
+      {
+        id: Date.now(),
+        title: todoTitle,
+        completed: false
+      }
+    ])
+    setTodoTitle(' ')
+  }
+}
+    return (
+      <div className="container">
+        <h1>Todo app</h1>
+
+          <div className="input-field">
+            <input 
+            value={todoTitle}
+            type="text" 
+            onChange={event => setTodoTitle(event.target.value)}
+            onKeyPress={addTodo}
+            />
+            <label>Todo name</label>
+          </div>
+
+          <TodoList todos={todos} />
+      </div>
+    );
+  }
+ 
